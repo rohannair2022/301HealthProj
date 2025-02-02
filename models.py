831c5e48b6ac1c882e8695e7644e5d7ef9656cd9
Andy Tran <aandy.tran@mail.utoronto.ca>
2025-02-01 23:46:43 -0500

    SUP 10 - Database Documentation
    Updated documentations for the newly created a email attribute for Patient model.

--- models.py
+++ models.py
@@ -15,6 +15,7 @@ class Patient(db.Model):
 
     u_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text, nullable=False)
+    email = db.Column(db.Text, nullable=False)
     password = db.Column(db.Text, nullable=False)
     avg_heartrate = db.Column(db.Integer)
     heart_score = db.Column(db.Integer, default=0)
@@ -45,7 +46,7 @@ class Friendship(db.Model):
 def create_patient():
     data = request.get_json()
 
-    if 'name' not in data or 'password' not in data:
+    if ('name' not in data) or ('password' not in data) or ('email' not in data):
         return jsonify({"error": "Missing required fields"}), 400
 
     new_patient = Patient(
@@ -86,7 +87,7 @@ def add_friend():
     u_id_1 = data.get('u_id_1')
     u_id_2 = data.get('u_id_2')
 
-    if not u_id_1 or not u_id_2:
+    if (not u_id_1) or (not u_id_2):
         return jsonify({"error": "Both u_id_1 and u_id_2 must be provided"}), 400
 
     if u_id_1 == u_id_2:
@@ -96,7 +97,7 @@ def add_friend():
     patient_1 = Patient.query.get(u_id_1)
     patient_2 = Patient.query.get(u_id_2)
 
-    if not patient_1 or not patient_2:
+    if (not patient_1) or (not patient_2):
         return jsonify({"error": "One or both patients do not exist"}), 404
 
     existing_friendship = Friendship.query.filter_by(patient_id=u_id_1, friend_id=u_id_2).first()
@@ -132,21 +133,21 @@ def remove_friend():
     u_id_1 = data.get('u_id_1')
     u_id_2 = data.get('u_id_2')
 
-    if not u_id_1 or not u_id_2:
+    if (not u_id_1) or (not u_id_2):
         return jsonify({"error": "Both u_id_1 and u_id_2 are required"}), 400
     
     # Check for if both users exist
     patient1 = Patient.query.get(u_id_1)
     patient2 = Patient.query.get(u_id_2)
 
-    if not patient1 or not patient2:
+    if (not patient1) or (not patient2):
         return jsonify({"error": "One or both patients not found"}), 404
 
     # Check for if both users are friended
     friendship1 = Friendship.query.filter_by(patient_id=u_id_1, friend_id=u_id_2).first()
     friendship2 = Friendship.query.filter_by(patient_id=u_id_2, friend_id=u_id_1).first()
 
-    if not friendship1 or not friendship2:
+    if (not friendship1) or (not friendship2):
         return jsonify({"error": "Friendship does not exist"}), 404
 
     db.session.delete(friendship1)
